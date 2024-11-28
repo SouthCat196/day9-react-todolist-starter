74e2820205a3bda0380b496d4c90fd209b885259
Sylas Qiu <qiusy@oocl.com>
2024-11-28 19:48:47 +0800

    [TodoList] feat: add todo item delete button

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,7 +1,19 @@
+import {useContext} from "react";
+import {TodoContext} from "../App";
+
 const TodoItem = (props) => {
 
+    const {dispatch} = useContext(TodoContext);
+
+    const handleDelete = () => {
+        dispatch({type: "DELETE", payload: props.todoItem.id})
+    }
+
     return (
-        <div>{props.todoItem.text}</div>
+        <div>
+            {props.todoItem.text}
+            <button onClick={handleDelete}>X</button>
+        </div>
     )
 }
 
