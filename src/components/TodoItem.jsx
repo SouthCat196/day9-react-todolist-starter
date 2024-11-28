9f73224a9661a0364245d2bfdb1a8b69ecbbc21b
Sylas Qiu <qiusy@oocl.com>
2024-11-28 20:13:19 +0800

    [TodoList] feat: add todo item handleToggleCompletion function

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,17 +1,25 @@
 import {useContext} from "react";
 import {TodoContext} from "../App";
+import styles from "../css/Todoitem.module.css"
 
 const TodoItem = (props) => {
+
+    const {id, text, done} = props.todoItem;
 
     const {dispatch} = useContext(TodoContext);
 
     const handleDelete = () => {
-        dispatch({type: "DELETE", payload: props.todoItem.id})
+        dispatch({type: "DELETE", payload: id})
+    }
+
+    const handleToggleCompletion = () => {
+        dispatch({type: "TOGGLE", payload: id})
     }
 
     return (
         <div>
-            {props.todoItem.text}
+            <span className={done ? styles.done : styles.not_done}
+                  onClick={handleToggleCompletion}>{text}</span>
             <button onClick={handleDelete}>X</button>
         </div>
     )
--- src/context/todoReducer.js
+++ src/context/todoReducer.js
@@ -1,14 +1,26 @@
 export const initialState = [
-    {id: Date.now() + Math.random(), text: "the first todo", done: false},
-    {id: Date.now() + Math.random(), text: "the second todo", done: false},
+    {id: Math.random(), text: "the first todo", done: false},
+    {id: Math.random(), text: "the second todo", done: false},
 ];
 
 export const todoReducer = (state, action) => {
     switch (action.type) {
         case "ADD":
-            return [...state, {id: Date.now(), text: action.payload, done: false}]
+            return [...state, {id: Math.random(), text: action.payload, done: false}]
         case "DELETE":
             return state.filter(todo => todo.id !== action.payload);
+        case "TOGGLE":
+            // return state.map((todo) =>
+            //     todo.id === action.payload.id ? { ...todo, done: !todo.done } : todo
+            // );
+            console.log(state);
+            console.log(action.payload);
+            return state.map((todo) => {
+                if (todo.id === action.payload) {
+                    return {...todo, done: !todo.done}
+                }
+                return todo
+            })
         default:
             return state;
     }
