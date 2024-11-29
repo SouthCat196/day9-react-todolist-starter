5ad6ece70263df78660143ac8c25c580267a98d5
Sylas Qiu <qiusy@oocl.com>
2024-11-29 11:16:46 +0800

    [TodoList] feat: add router for todo list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
 import TodoList from "./components/TodoList";
 import {TodoProvider} from "./context/TodoContext"
+import {BrowserRouter as Router, Link, Navigate, Route, Routes} from "react-router-dom";
+import NotFound from "./components/NotFound";
+import DoneList from "./components/DoneList";
 
 function App() {
     return (
         <div className="App">
             <TodoProvider>
-                <TodoList/>
+                <Router>
+                    <nav>
+                        <Link to={"/todo-list"}>Home</Link> | <Link to={"/done-list"}>Done List</Link>
+                    </nav>
+                    <Routes>
+                        <Route path={"/"} element={<Navigate to={"/todo-list"}/>}/>
+                        <Route path={"/todo-list"} element={<TodoList/>}/>
+                        <Route path={"/done-list"} element={<DoneList/>}/>
+                        <Route path={"*"} element={<NotFound/>}/>
+                    </Routes>
+                </Router>
             </TodoProvider>
         </div>
     );
---
+++ src/components/DoneList.jsx
@@ -0,0 +1,17 @@
+import {useContext} from "react";
+import {TodoContext} from "../context/TodoContext";
+
+const DoneList = () => {
+    const {state} = useContext(TodoContext);
+    const doneList = state.filter((todo) => todo.done);
+
+    return (
+        <div>
+            {doneList.map((todo) => {
+                return <div>{todo.text}</div>
+            })}
+        </div>
+    )
+}
+
+export default DoneList;
