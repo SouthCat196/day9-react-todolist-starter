0483e8503b7fb72e5d5cd9d9a8a898bee7abb6b3
Sylas Qiu <qiusy@oocl.com>
2024-11-29 09:22:35 +0800

    [TodoList] refactor: change data id random to date

--- src/context/todoReducer.js
+++ src/context/todoReducer.js
@@ -1,14 +1,11 @@
 import {ADD, DELETE, TOGGLE} from "../constant/TodoListConstant";
 
-export const initialState = [
-    {id: Math.random(), text: "the first todo", done: false},
-    {id: Math.random(), text: "the second todo", done: false},
-];
+export const initialState = [];
 
 export const todoReducer = (state, action) => {
     switch (action.type) {
         case ADD:
-            return [...state, {id: Math.random(), text: action.payload, done: false}]
+            return [...state, {id: Date.now(), text: action.payload, done: false}]
         case DELETE:
             return state.filter(todo => todo.id !== action.payload);
         case TOGGLE:
